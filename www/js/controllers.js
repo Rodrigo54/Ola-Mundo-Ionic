881d771023131782ec1a14c134ad1a8aee970518
Rodrigo Alves <rodrigo54mix@gmail.com>
2017-01-06 21:31:53 -0300

    update package.json

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,28 @@
-angular.module('starter.controllers', [])
+angular.module('starter')
 
-.controller('HomeCtrl', function($scope) {})
+.controller('HomeCtrl', function($scope, $ionicPlatform, $cordovaInAppBrowser) {
+
+  var options = {
+      location: 'yes',
+      clearcache: 'yes',
+      // toolbar: 'no'
+    };
+
+  $scope.openBrowser = function() {
+    $ionicPlatform.ready(function () {
+      $cordovaInAppBrowser.open('http://rodrigom.tk/', '_system', options)
+        .then(function(event) {
+          // success
+        })
+        .catch(function(event) {
+          // error
+        });
+
+      $cordovaInAppBrowser.close();
+    });
+
+      // $cordovaInAppBrowser.open('http://rodrigom.tk/', '_system', options);
+      // window.open('http://rodrigom.tk/','_blank','location=yes');
+  }
+})
 .controller('SobreCtrl', function($scope) {});
